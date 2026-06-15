d57846b39e927b024b9d265b17d87722daf73348
Sebas2411 <mastropiero2411@gmail.com>
2026-06-14 22:26:59 -0300

    actualizamos para importar desde react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home.jsx";
 import Register from "./pages/register/register.jsx";
 import Login from "./pages/login/login.jsx";
