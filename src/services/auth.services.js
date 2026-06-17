00858a7a3f682201816837b205fc79f297670fd8
Mariana L <marianaluis2011@gmail.com>
2026-06-16 21:55:05 -0300

    conecto fe y be login y register

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/auth";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const registerUser = async (userData) => {
   const response = await axios.post(
