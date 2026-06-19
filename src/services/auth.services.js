e65398628ca8d0af309e0e2e1b93b5f6c91cb183
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-06-19 13:11:43 -0300

    feat: conectar login y registro con el backend

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -3,29 +3,12 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const registerUser = async (userData) => {
-  const response = await axios.post(
-    `${API_URL}/register`,
-    userData
-  );
-
-  return response.data;
+  const { data } = await axios.post(`${API_URL}/register`, userData);
+  return data;
 };
 
 export const loginUser = async (credentials) => {
-  const response = await fetch(`${API_URL}/auth/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  });
-
-  const data = await response.json();
-
-  if (!response.ok) {
-    throw new Error(data.message);
-  }
-
+  const { data } = await axios.post(`${API_URL}/login`, credentials);
   return data;
 };
 
