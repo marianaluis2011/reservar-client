718eb1cdc37a42cfe67321161f3cf9f62380c1d3
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-06-23 14:05:09 -0300

    feat: creo route en AppRoute jsx para que solo y solo el super admin pueda entrar a su panel

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -27,11 +27,13 @@ export default function AppRouter() {
         <Route path="/about" element={<About />} />
 
         {/* Privadas */}
-        <Route element={<ProtectedRoute allowedRoles={["host", "super_admin"]} />}>
-        <Route path="/host/dashboard" element={<HostDashboard />} />
+<Route element={<ProtectedRoute allowedRoles={["host"]} />}>
+  <Route path="/host/dashboard" element={<HostDashboard />} />
+</Route>
 
-        <Route path="/host/superAdmin" element={<SuperAdminDashboard />} />
-      </Route>
+<Route element={<ProtectedRoute allowedRoles={["super_admin"]} />}>
+  <Route path="/host/superAdmin" element={<SuperAdminDashboard />} />
+</Route>
 
         {/* Error */}
         <Route path="*" element={<Error404 />} />
