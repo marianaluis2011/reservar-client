2fafd48d0bd83f15379d4e4789436010c80548ac
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-06-23 14:53:53 -0300

    feat: creo nuevo servicio host services

---
+++ src/services/host.services.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
+const API_BASE = API_URL.replace("/auth", "");
+const getToken = () => localStorage.getItem("token") || sessionStorage.getItem("token");
+const authHeader = () => ({ headers: { Authorization: `Bearer ${getToken()}` } });
+
+export const getMyAccommodation = async () => {
+  const { data } = await axios.get(`${API_BASE}/hospedajes/owner/me`, authHeader());
+  return data;
+};
+
+export const getRoomsByAccommodation = async (accommodationId) => {
+  const { data } = await axios.get(`${API_BASE}/habitaciones/hospedaje/${accommodationId}`);
+  return data;
+};
