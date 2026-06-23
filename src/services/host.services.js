7f16d47edf2d36320168aba196aea044aaa2f689
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-06-23 15:22:04 -0300

    refactor: dejo mejor identado

--- src/services/host.services.js
+++ src/services/host.services.js
@@ -5,16 +5,16 @@ const getToken = () => localStorage.getItem("token") || sessionStorage.getItem("
 const authHeader = () => ({ headers: { Authorization: `Bearer ${getToken()}` } });
 
 export const getMyAccommodation = async () => {
-  const { data } = await axios.get(`${API_BASE}/hospedajes/owner/me`, authHeader());
-  return data;
+    const { data } = await axios.get(`${API_BASE}/hospedajes/owner/me`, authHeader());
+    return data;
 };
 
 export const getRoomsByAccommodation = async (accommodationId) => {
-  const { data } = await axios.get(`${API_BASE}/habitaciones/hospedaje/${accommodationId}`);
-  return data;
+    const { data } = await axios.get(`${API_BASE}/habitaciones/hospedaje/${accommodationId}`);
+    return data;
 };
 
 export const getOwnerBookings = async () => {
-  const { data } = await axios.get(`${API_BASE}/reservas/owner`, authHeader());
-  return data;
+    const { data } = await axios.get(`${API_BASE}/reservas/owner`, authHeader());
+    return data;
 };
