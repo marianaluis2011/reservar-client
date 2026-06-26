e623cf52a955b238a1ea092da8ebd16ad3a00663
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-06-25 21:10:37 -0300

    feat: creo archivo de servicio para bookings

---
+++ src/services/booking.services.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const API_BASE = API_URL.replace("/auth", "");
+const getToken = () => localStorage.getItem("token") || sessionStorage.getItem("token");
+const authHeader = () => ({ headers: { Authorization: `Bearer ${getToken()}` } });
+
+export const createBooking = async (bookingData) => {
+  const { data } = await axios.post(`${API_BASE}/reservas`, bookingData, authHeader());
+  return data;
+};
