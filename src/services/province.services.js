443eb52e8bb0e7e0b0196cc4b4a7dcb9fb9c207e
Mariana L <marianaluis2011@gmail.com>
2026-06-29 07:55:19 -0300

    quito /auth en url y quito replace en servicios

--- src/services/province.services.js
+++ src/services/province.services.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
-const API_BASE = API_URL.replace("/auth", "");
+
+// const API_BASE = API_URL.replace("/auth", "");
 
 export const getProvinces = async () => {
   const { data } = await axios.get(`${API_BASE}/provincias`);
